6252c93b2c4a6a9c9526b1dd435ed656db828ab0
Daniel Wade <dcwade87@gmail.com>
2016-01-27 13:53:45 -0800

    Finally got render to work with for loops

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var pikePlace = new CookieStand ("Pike Place", 17, 88, 5.2);
 var seaTac = new CookieStand ("SeaTac", 6, 24, 1.2);
 var southcenter = new CookieStand ("Southcenter", 11, 38, 1.9);
-var bellevueSquare = new CookieStand ("Bellevue Square");
+var bellevueSquare = new CookieStand ("Bellevue Square", 8, 20, 48, 3.3);
 var alki = new CookieStand ("Alki", 3, 24, 2.6);
 var hoursOpen = ["10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
@@ -25,3 +25,28 @@ CookieStand.prototype.hourlySales = function () {
     this.dailySales += random;
   }
 };
+
+CookieStand.prototype.render = function() {
+    this.hourlySales();
+    var printCookie = document.getElementById('cookieList');
+    var cookieSection = document.createElement('section');
+    printCookie.appendChild(cookieSection);
+    var ulEl = document.createElement("ul");
+
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[i] + ": " + this.cookieArr[i];
+      ulEl.appendChild(liEl);
+    }
+    var liElTotal = document.createElement("li");
+    liElTotal.textContent = "Daily Sales: " + this.dailySales;
+    ulEl.appendChild(liElTotal);
+    cookieSection.textContent = this.storeLocation;
+    cookieSection.appendChild(ulEl);
+  }
+
+pikePlace.render();
+seaTac.render();
+southcenter.render();
+bellevueSquare.render();
+alki.render();
